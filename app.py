317dda2f2d338a991dffa986a62bdc49c5774581
yjawais <yjawais@gmail.com>
2022-11-16 22:17:09 +0530

    basix

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-# import os
-# import numpy as np
-# from PIL import Image
-# import cv2
-# from keras.models import load_model
+import os
+import numpy as np
+from PIL import Image
+import cv2
+from keras.models import load_model
 from flask import Flask,request,render_template
-# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 
 
-# model =load_model('BrainTumor10Epochs.h5')
+model =load_model('BrainTumor10Epochs.h5')
 
 
-# def get_className(classNo):
-# 	if classNo==0:
-# 		return "No Brain Tumor"
-# 	elif classNo==1:
-# 		return "Yes Brain Tumor"
+def get_className(classNo):
+	if classNo==0:
+		return "No Brain Tumor"
+	elif classNo==1:
+		return "Yes Brain Tumor"
 
 
-# def getResult(img):
-#     image=cv2.imread(img)
-#     image = Image.fromarray(image, 'RGB')
-#     image = image.resize((64, 64))
-#     image=np.array(image)
-#     input_img = np.expand_dims(image, axis=0)
-#     result=model.predict(input_img)
-#     return result
+def getResult(img):
+    image=cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64, 64))
+    image=np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result=model.predict(input_img)
+    return result
 
 
 @app.route('/')
