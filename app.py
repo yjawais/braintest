2c032354adde9531f4b5bbdfa693a2a0eef41896
yjawais <yjawais@gmail.com>
2022-11-28 19:19:57 +0530

    used tf

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 from PIL import Image
-import cv2
-from keras.models import load_model
+# import cv2
+import tensorflow as tf
+# from keras.models import load_model
 from flask import Flask,request,render_template
 from werkzeug.utils import secure_filename
 
@@ -10,7 +11,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
-# model = load_model('BrainTumor10Epochs.h5')
+model = tf.keras.models.load_model('BrainTumor10Epochs.h5')
 
 
 def get_className(classNo):
